08dfbbcb34ad32348e455c308c6a2f96fc5f1191
AbdulR <rehman.a@gmail.com>
2015-01-31 18:28:43 -0500

    Initial commit 2

---
+++ ODataSandbox.Data/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ODataSandbox.Data.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ODataSandbox.Data/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ODataSandbox.Data.WebApiApplication" Language="C#" %>
