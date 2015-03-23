baef13924dbeb88c242d05bdb5c1d747936c3596
rehman.a@live.com <rehman.a@live.com>
2015-03-22 23:24:07 -0400

    Updated to March 22 nightly Replaced content negotiator

--- ODataSandbox.Data/App_Start/WebApiConfig.cs
+++ ODataSandbox.Data/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.OData.Builder;
 using System.Web.OData.Extensions;
@@ -23,6 +26,9 @@ namespace ODataSandbox.Data
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
+
             ODataModelBuilder builder = new ODataConventionModelBuilder();
             builder.EntitySet<Employee>("Employees");
             builder.EntitySet<Product>("Products");
@@ -33,6 +39,25 @@ namespace ODataSandbox.Data
                 routeName: "ODataRoute",
                 routePrefix: "odata",
                 model: builder.GetEdmModel());
+        }
+    }
+
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+            _jsonFormatter.SerializerSettings = new Newtonsoft.Json.JsonSerializerSettings{
+                Formatting = Newtonsoft.Json.Formatting.None
+            };
+        }
+
+        public ContentNegotiationResult Negotiate(Type type, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters)
+        {
+            var result = new ContentNegotiationResult(_jsonFormatter, new MediaTypeHeaderValue("application/json"));
+            return result;
         }
     }
 }
