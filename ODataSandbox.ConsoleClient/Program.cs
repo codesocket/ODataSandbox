012d715b429089bcad4b0833725b94037d0e3947
rehman.a@live.com <rehman.a@live.com>
2015-04-06 23:24:20 -0400

    Updating Program.cs

--- ODataSandbox.ConsoleClient/Program.cs
+++ ODataSandbox.ConsoleClient/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Default;
+using Microsoft.OData.Client;
+using ODataSandbox.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +16,14 @@ namespace ODataSandbox.ConsoleClient
             var containerV3 = new NorthwindEntitiesV3.NorthwindEntities(new Uri("http://localhost:52799/odata"));
             var containerV4 = new Default.NorthwindEntities(new Uri("http://localhost:53372/odata/"));
 
+            var product = new Product();
+
+            var query = new DataServiceQuerySingle<Product>(containerV4, "");
+
+            //containerV4.Products.ByKey
+
             var productV3 = containerV3.Products.First();
-            var productV4 = containerV4.Products.First();
+            var productV4 = containerV4.Orders.OrderBy(p => p.OrderDate).First();
         }
     }
 }
